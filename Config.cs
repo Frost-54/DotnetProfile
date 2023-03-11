7f48aa1882a05cc1cbb08d1d837b2cfca0589a1f
~ <~>
2023-03-11 18:14:52 +1000

    Add additional services

--- Config.cs
+++ Config.cs
@@ -1,11 +1,19 @@
+using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
 
 namespace DotnetProfile;
 
 public class Config : ManualConfig {
-      public Config(string artifactPath) {
+      public Config(string artifactPath, string name) {
             AddExporter(MarkdownExporter.GitHub);
-            ArtifactsPath = artifactPath;
+            AddDiagnoser(new IDiagnoser[] {
+                  new MemoryDiagnoser(new MemoryDiagnoserConfig()),
+            });
+            AddColumnProvider(DefaultColumnProviders.Instance);
+            AddLogger(new ConsoleLogger());
+            ArtifactsPath = Path.Join(artifactPath, name);
       }
 }
