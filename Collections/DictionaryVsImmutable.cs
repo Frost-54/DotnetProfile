7f92ff2f9d7cdf2f8c98385e3944acf9512043a3
~ <~>
2023-03-22 17:32:30 +1000

    ImmutableDictionary vs Dictionary

---
+++ Collections/DictionaryVsImmutable.cs
@@ -0,0 +1,60 @@
+using System.Collections.Immutable;
+using BenchmarkDotNet.Attributes;
+
+namespace DotnetProfile.Collections;
+
+[Description("C# immutable dictionary vs dictionary performance")]
+[BenchmarkClass("Collections")]
+public class DictionaryVsImmutable {
+      private static Dictionary<int, int> MakeDictionary(int size) {
+            var dict = new Dictionary<int, int>(size);
+
+            for (int i = 0; i < size; i++) {
+                  dict.Add(i, i);
+            }
+            return dict;
+      }
+
+      private static IEnumerable<object[]> GetDictionary() {
+            return new object[][] {
+                  new object[] { MakeDictionary(0),0 },
+                  new object[] { MakeDictionary(10),10 },
+                  new object[] { MakeDictionary(100),100 },
+                  new object[] { MakeDictionary(1000),1000 },
+                  new object[] { MakeDictionary(1000),1000 },
+                  new object[] { MakeDictionary(10000),10000 },
+            };
+      }
+
+      [ArgumentsSource(nameof(GetDictionary))]
+      [Benchmark]
+      public static void DictionaryPerformance(Dictionary<int, int> dictionary, int next) {
+            dictionary.Add(next, next);
+      }
+
+      private static ImmutableDictionary<int, int> MakeImmutableDictionary(int size) {
+            var dict = ImmutableDictionary.CreateBuilder<int, int>();
+
+            for (int i = 0; i < size; i++) {
+                  dict.Add(i, i);
+            }
+            return dict.ToImmutable();
+      }
+
+      private static IEnumerable<object[]> GetImmutableDictionary() {
+            return new object[][] {
+                  new object[] { MakeImmutableDictionary(0),0 },
+                  new object[] { MakeImmutableDictionary(10),10 },
+                  new object[] { MakeImmutableDictionary(100),100 },
+                  new object[] { MakeImmutableDictionary(1000),1000 },
+                  new object[] { MakeImmutableDictionary(1000),1000 },
+                  new object[] { MakeImmutableDictionary(10000),10000 },
+            };
+      }
+
+      [ArgumentsSource(nameof(GetImmutableDictionary))]
+      [Benchmark]
+      public static void ImmutableDictionaryPerformance(ImmutableDictionary<int, int> dictionary, int next) {
+            dictionary.Add(next, next);
+      }
+}
