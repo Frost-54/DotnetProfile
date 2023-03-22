85905b24f244b4b529c0a0a4cc52191b0e33c94a
~ <~>
2023-03-22 17:33:41 +1000

    MethodInfo.Invoke performance

---
+++ Function/ReflectionInvoke.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using BenchmarkDotNet.Attributes;
+
+namespace DotnetProfile.Function;
+
+[BenchmarkClass("Reflection")]
+[Description("c# MethodInfo.Invoke performance")]
+public class ReflectionInvoke {
+      private readonly Action function = () => {};
+      private readonly MethodInfo methodInfo;
+
+      public ReflectionInvoke() {
+            methodInfo = function.Method;
+      }
+
+      [Benchmark]
+      public void ReflectionInvokePerformance() {
+            methodInfo.Invoke(null, Array.Empty<object>());
+      }
+}
