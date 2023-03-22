3bbb1ab490039be53317110946417be18653d210
~ <~>
2023-03-22 17:32:18 +1000

    Dictionary performance

---
+++ Collections/DictionaryPerformance.cs
@@ -0,0 +1,33 @@
+using BenchmarkDotNet.Attributes;
+
+namespace DotnetProfile.Collections;
+
+[Description("C# Dictionary performance")]
+[BenchmarkClass("Collections")]
+public class DictionaryPerformance {
+      private static Dictionary<int, int> MakeDictionary(int size) {
+            var dict = new Dictionary<int, int>(size);
+
+            for (int i = 0; i < size; i++) {
+                  dict.Add(i, i);
+            }
+            return dict;
+      }
+
+      private static IEnumerable<object[]> GetDictionary() {
+            return new object[][] {
+                  new object[] { MakeDictionary(0),0 },
+                  new object[] { MakeDictionary(10),10 },
+                  new object[] { MakeDictionary(100),100 },
+                  new object[] { MakeDictionary(1000),1000 },
+                  new object[] { MakeDictionary(1000),1000 },
+                  new object[] { MakeDictionary(10000),10000 },
+            };
+      }
+
+      [ArgumentsSource(nameof(GetDictionary))]
+      [Benchmark]
+      public static void Performance(Dictionary<int, int> dictionary, int next) {
+            dictionary.Add(next, next);
+      }
+}
